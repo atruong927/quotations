a6bb17ee39ff5e38b91de9c890d6026734fa8254
atruong927 <atruong927@gmail.com>
2015-10-16 11:46:12 -0400

    Initial Commit

---
+++ QuotationsApp/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QuotationsApp.Models
+{
+    public class Category
+    {
+        public int CategoryID
+        {
+            get; set;
+        }
+        public string Name
+        {
+            get; set;
+        }
+        //Navigation property goes 
+    }
+}
---
+++ QuotationsApp/Models/Quotation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace QuotationsApp.Models
+{
+    public class Quotation
+    {
+        public int QuotationID
+        {
+            get; set;
+        }
+        public string Author
+        {
+            get; set;
+        }
+        public string Quote
+        {
+            get; set;
+        }
+        [Display(Name = "Date Added")]
+        [DataType(DataType.Date)]
+        public DateTime DateAdded
+        {
+            get; set;
+        }
+        public int CategoryID
+        {
+            get; set;
+        }
+        public virtual Category Category
+        {
+            get; set;
+        }
+    }
+}
